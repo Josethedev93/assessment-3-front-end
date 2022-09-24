174bfb2d41057d2de5fb88b1ebe3c6ea88d93922
hernj07 <Hernandezj5026@gmail.com>
2022-09-23 20:45:33 -0500

    commit two

--- about.js
+++ about.js
@@ -5,10 +5,19 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	// console.log('form submit');
+	alert ('form has been successfully submitted')
 }
 
 
 let form = document.querySelector('#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+const logo = document.querySelector('.logo');
+
+function logoevent() {
+	alert('congrats! you know my secret!')
+}
+
+logo.addEventListener('mouseover', logoevent)
